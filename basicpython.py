1cf823da1c21ea17457c9304191a1bb4b23d3673
motlatsi100-hub <motzrootz@gmail.com>
2025-08-14 17:54:23 +0200

    Add files via upload

---
+++ basicpython.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 14 00:18:56 2025
+
+@author: motlatsi seeko
+"""
+
+# Basic Calculator Program
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+# Perform calculation
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation. Please use +, -, *, or /.")
